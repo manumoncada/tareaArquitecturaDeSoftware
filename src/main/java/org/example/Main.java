7c026f2d9f9a18a217f19ffbc6e57cc93d51652e
manuel <moncadajuanmanuel25@gmail.com>
2025-02-18 21:20:05 -0500

    ejerciciotarea

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("git config");
+        System.out.print("hi");
+        System.out.print("mother fucker");
+    }
+}
